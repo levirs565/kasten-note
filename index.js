de928526b45a6a07d9da974f7263598fac181e51
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-04-10 09:15:41 +0700

    support for processing index file

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const path = require("path")
 const fs = require("fs").promises
+const unified = require("unified")
+const markdown = require("remark-parse")
+const remark2rehype = require("remark-rehype")
+const html = require("rehype-stringify")
+const vfile = require("to-vfile")
+const reporter = require("vfile-reporter")
 
 const configFileName = "kasten.yml"
 
@@ -9,13 +15,13 @@ async function getCurrentDir() {
   for (let i = dirs.length; i >= 0; i--) {
     const dir = dirs.slice(0, i).join(path.sep)
     const configFile = path.join(dir, configFileName)
-    try { 
+    try {
       const fileStat = await fs.stat(configFile)
       if (fileStat.isFile) {
         currentDir = dir
         break
       }
-    } catch(e) {
+    } catch (e) {
     }
   }
 
@@ -24,5 +30,19 @@ async function getCurrentDir() {
 }
 
 (async () => {
-  console.log(await getCurrentDir())
+  const dir = await getCurrentDir()
+  const distDir = path.join(dir, "dist")
+  const file = path.join(dir, "index.md")
+  const processor = unified()
+    .use(markdown)
+    .use(remark2rehype)
+    .use(html)
+
+  const resultFile = await processor.process(await vfile.read(file))
+  console.log(reporter(resultFile))
+  resultFile.dirname = distDir
+  resultFile.extname = ".html"
+
+  fs.mkdir(resultFile.dirname, { recursive: true })
+  await vfile.write(resultFile)
 })()
