dd5b684f7dfe56a97499083458ecf81a4bca9d96
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-04-11 19:19:31 +0700

    add serve command

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const { program } = require("commander")
 const build = require("./src/build")
+const serve = require("./src/serve")
 
 program
   .name("kasten")
   .version("0.1")
 
-program .command("build")
+const cleanOpt = ["--no-clean", "clean dist directory before build"]
+
+program.command("build")
   .description("build all notes to HTML")
-  .option("--no-clean", "clean dist directory before build")
+  .option(...cleanOpt)
   .option("-w, --watch", "watch directory for change", false)
   .action(build.buildDir)
+
+program.command("serve")
+  .description("build and serve notes")
+  .option(...cleanOpt)
+  .action(serve.serveDir)
 
 program.parseAsync(process.argv)
