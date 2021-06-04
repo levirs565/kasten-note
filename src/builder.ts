994ffea5dece323de1fb96195765b3a794eaacd5
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-06-04 15:11:18 +0700

    refractor: split file reading and writing from pipeline

--- src/builder.ts
+++ src/builder.ts
@@ -1,160 +1,177 @@
-import chokidar from "chokidar"
-import fs from "fs-extra"
-import { NoteList } from "./base"
-import * as util from "./util"
-import * as pipeline from "./pipeline"
-import { NodeId, default as createGraph } from "ngraph.graph"
-import toJson from "ngraph.tojson"
-import { join } from "path"
+import chokidar from 'chokidar';
+import fs from 'fs-extra';
+import { NoteList } from './base';
+import * as util from './util';
+import * as pipeline from './pipeline';
+import { NodeId, default as createGraph } from 'ngraph.graph';
+import toJson from 'ngraph.tojson';
+import { join } from 'path';
+import toVfile from 'to-vfile';
+import reporter from "vfile-reporter"
 
 function isMarkdown(p: string) {
-  return p.endsWith(".md")
+  return p.endsWith('.md');
 }
 
-
 export default class Builder {
-  kastenDir: string
-  clean: boolean
-  watch: boolean
-  verbose: boolean
-  onUpdate!: (fileName: string) => void
-  noteList = new NoteList()
-  pendingBuild = new Array<string>()
-  isReady = false
-  onAfterReady!: () => void
-  imgGraph = createGraph()
-  watcher!: chokidar.FSWatcher
+  kastenDir: string;
+  clean: boolean;
+  watch: boolean;
+  verbose: boolean;
+  onUpdate!: (fileName: string) => void;
+  noteList = new NoteList();
+  pendingBuild = new Array<string>();
+  isReady = false;
+  onAfterReady!: () => void;
+  imgGraph = createGraph();
+  watcher!: chokidar.FSWatcher;
 
   constructor(dir: string, clean: boolean, watch: boolean, verbose: boolean) {
-    this.kastenDir = dir
-    this.clean = clean
-    this.watch = watch
-    this.verbose = verbose
+    this.kastenDir = dir;
+    this.clean = clean;
+    this.watch = watch;
+    this.verbose = verbose;
   }
 
   private maybeUpdate(path: string) {
-    if (this.onUpdate)
-      this.onUpdate(path)
+    if (this.onUpdate) this.onUpdate(path);
   }
 
-  private async rebuild(path: string) {
-    if (this.verbose)
-      console.log(`Building ${path}`)
-    const lastImgList: NodeId[] = []
+  private wikiLinkResolver = (name: string) => [
+    this.noteList.getById(name)?.urlPath,
+  ];
 
-    this.imgGraph.forEachLinkedNode(path, (node, link) => {
-      lastImgList.push(link.toId)
-      this.imgGraph.removeLink(link)
-    }, true)
+  private async rebuild(path: string): Promise<void> {
+    if (this.verbose) console.log(`Building ${path}`);
+    const lastImgList: NodeId[] = [];
 
-    await pipeline.buildMarkdown(this.kastenDir, path, this.noteList, this.onImageFound, !this.verbose)
+    this.imgGraph.forEachLinkedNode(
+      path,
+      (node, link) => {
+        lastImgList.push(link.toId);
+        this.imgGraph.removeLink(link);
+      },
+      true
+    );
+
+    const resultFile = await pipeline.buildMarkdown(
+      await toVfile.read({ cwd: this.kastenDir, path }),
+      this.wikiLinkResolver,
+      this.onImageFound
+    );
+    resultFile.path = util.getDistFile(this.kastenDir, path)
+    await fs.ensureFile(resultFile.path!)
+    await toVfile.write(resultFile)
+
+    const report = reporter(resultFile, { quiet: !this.verbose });
+    if (report.length > 0) console.log(report);
 
     for (const img of lastImgList) {
-      if (this.imgGraph.getLinks(img)!.length > 0)
-        continue
+      if (this.imgGraph.getLinks(img)!.length > 0) continue;
 
-      this.imgGraph.removeNode(img)
-      this.watcher.unwatch(img as string)
+      this.imgGraph.removeNode(img);
+      this.watcher.unwatch(img as string);
       try {
-        this.removeAsset(img as string)
-      } catch {
-      }
+        this.removeAsset(img as string);
+      } catch {}
     }
 
-    this.notifyPageChanged(path)
+    this.notifyPageChanged(path);
   }
   notifyPageChanged(path: string) {
-    this.maybeUpdate(util.getDistName(path))
+    this.maybeUpdate(util.getDistName(path));
   }
 
   private onImageFound = (md: string, img: string) => {
     if (!this.imgGraph.hasLink(md, img)) {
-      this.imgGraph.addLink(md, img)
-      this.updateAsset(img)
+      this.imgGraph.addLink(md, img);
+      this.updateAsset(img);
     }
-  }
+  };
 
   private onChange(path: string) {
     if (isMarkdown(path)) {
-      this.rebuild(path)
+      this.rebuild(path);
     } else if (this.isAsset(path)) {
-      this.updateAsset(path)
-      this.notifyAssetChanged(path)
+      this.updateAsset(path);
+      this.notifyAssetChanged(path);
     }
   }
 
   private isAsset(path: string) {
-    return this.imgGraph.hasNode(path)
+    return this.imgGraph.hasNode(path);
   }
 
   private notifyAssetChanged(path: string) {
-    this.imgGraph.forEachLinkedNode(path, (node, link) => {
-      this.notifyPageChanged(node.id as string)
-    }, false)
+    this.imgGraph.forEachLinkedNode(
+      path,
+      (node, link) => {
+        this.notifyPageChanged(node.id as string);
+      },
+      false
+    );
   }
 
   private updateAsset(path: string) {
-    if (this.verbose)
-      console.log(`Updating asset ${path}`)
-    fs.copySync(join(this.kastenDir, path), join(util.getDistDir(this.kastenDir), path))
+    if (this.verbose) console.log(`Updating asset ${path}`);
+    fs.copySync(
+      join(this.kastenDir, path),
+      join(util.getDistDir(this.kastenDir), path)
+    );
   }
 
   private removeAsset(path: string) {
-    if (this.verbose)
-      console.log(`Removing asset ${path}`)
-    fs.removeSync(join(util.getDistDir(this.kastenDir), path))
+    if (this.verbose) console.log(`Removing asset ${path}`);
+    fs.removeSync(join(util.getDistDir(this.kastenDir), path));
   }
 
   private onAdd(path: string) {
     if (isMarkdown(path)) {
-      this.noteList.addFile(path)
-      if (this.isReady)
-        this.rebuild(path)
-      else this.pendingBuild.push(path)
+      this.noteList.addFile(path);
+      if (this.isReady) this.rebuild(path);
+      else this.pendingBuild.push(path);
     } else if (this.isAsset(path)) {
-      this.updateAsset(path)
-      this.notifyAssetChanged(path)
+      this.updateAsset(path);
+      this.notifyAssetChanged(path);
     }
   }
 
   private onUnlink(path: string) {
     if (isMarkdown(path)) {
-      this.noteList.removeFile(path)
-      fs.removeSync(util.getDistFile(this.kastenDir, path))
-      this.maybeUpdate(util.getDistName(path))
+      this.noteList.removeFile(path);
+      fs.removeSync(util.getDistFile(this.kastenDir, path));
+      this.maybeUpdate(util.getDistName(path));
     } else if (this.isAsset(path)) {
-      this.removeAsset(path)
-      this.notifyAssetChanged(path)
+      this.removeAsset(path);
+      this.notifyAssetChanged(path);
     }
   }
 
   onReady = async () => {
-    this.isReady = true
-    if (this.verbose)
-      console.log("Builder is ready now")
+    this.isReady = true;
+    if (this.verbose) console.log('Builder is ready now');
     for (const file of this.pendingBuild) {
-      await this.rebuild(file)
+      await this.rebuild(file);
     }
-    if (this.onAfterReady)
-      this.onAfterReady()
-  }
+    if (this.onAfterReady) this.onAfterReady();
+  };
 
   async run() {
-    if (this.clean)
-      await fs.emptyDir(util.getDistDir(this.kastenDir))
+    if (this.clean) await fs.emptyDir(util.getDistDir(this.kastenDir));
 
-    this.watcher = chokidar.watch("**/*", {
-      cwd: this.kastenDir,
-      persistent: this.watch,
-      ignored: util.excludedFiles
-    })
-      .on("change", this.onChange.bind(this))
-      .on("add", this.onAdd.bind(this))
-      .on("unlink", this.onUnlink.bind(this))
-      .on("ready", this.onReady)
+    this.watcher = chokidar
+      .watch('**/*', {
+        cwd: this.kastenDir,
+        persistent: this.watch,
+        ignored: util.excludedFiles,
+      })
+      .on('change', this.onChange.bind(this))
+      .on('add', this.onAdd.bind(this))
+      .on('unlink', this.onUnlink.bind(this))
+      .on('ready', this.onReady);
   }
 
   async stop() {
-    await this.watcher.close()
+    await this.watcher.close();
   }
 }
--- src/pipeline.ts
+++ src/pipeline.ts
@@ -1,68 +1,73 @@
-import path from "path"
-import fs from "fs-extra"
-import { NoteList } from "./base"
-import * as util from "./util"
-import unified from "unified"
-import markdown from "remark-parse"
-import gfm from "remark-gfm"
-import math from "remark-math"
-import { wikiLinkPlugin } from "remark-wiki-link"
-import remark2rehype from "remark-rehype"
-import raw from "rehype-raw"
-import katex from "rehype-katex"
-import document from "rehype-document"
-import html from "rehype-stringify"
-import toVfile from "to-vfile"
-import reporter from "vfile-reporter"
-import visit from "unist-util-visit"
-import mdastToString from "mdast-util-to-string"
-import unist from "unist"
-import { Heading, Image } from "mdast"
-import vfile from "vfile"
+import path from 'path';
+import fs from 'fs-extra';
+import { NoteList } from './base';
+import * as util from './util';
+import unified from 'unified';
+import markdown from 'remark-parse';
+import gfm from 'remark-gfm';
+import math from 'remark-math';
+import { wikiLinkPlugin } from 'remark-wiki-link';
+import remark2rehype from 'remark-rehype';
+import raw from 'rehype-raw';
+import katex from 'rehype-katex';
+import document from 'rehype-document';
+import html from 'rehype-stringify';
+import toVfile from 'to-vfile';
+import reporter from 'vfile-reporter';
+import visit from 'unist-util-visit';
+import mdastToString from 'mdast-util-to-string';
+import unist from 'unist';
+import { Heading, Image } from 'mdast';
+import vfile from 'vfile';
 
-type ImageFoundListener = (md: string, img: string) => void
+type ImageFoundListener = (md: string, img: string) => void;
 
-function analyzerAttacker(doc: document.Options, onImageFound: ImageFoundListener) {
-  return analyzer
+function analyzerAttacker(
+  doc: document.Options,
+  onImageFound: ImageFoundListener
+) {
+  return analyzer;
 
   function analyzer(tree: unist.Node, file: vfile.VFile) {
-    visit(tree, ["heading", "image"], visitor)
+    visit(tree, ['heading', 'image'], visitor);
 
-    function visitor(node: unist.Node, index: number, parent: unist.Node | undefined) {
-      if (node.type == "heading") {
+    function visitor(
+      node: unist.Node,
+      index: number,
+      parent: unist.Node | undefined
+    ) {
+      if (node.type == 'heading') {
         if ((node as Heading).depth == 1) {
-          doc.title = mdastToString(node)
+          doc.title = mdastToString(node);
         }
-      } else if (node.type == "image") {
-        const image = node as Image
-        if (image.url.indexOf("://") >= 0)
-          return
+      } else if (node.type == 'image') {
+        const image = node as Image;
+        if (image.url.indexOf('://') >= 0) return;
 
-        let imgPath = image.url
-        if (imgPath[0] == '/')
-          imgPath = imgPath.substring(1)
-        else
-          imgPath = path.join(file.dirname!!, imgPath)
-        imgPath = path.normalize(imgPath)
-        onImageFound(file.path!, imgPath)
+        let imgPath = image.url;
+        if (imgPath[0] == '/') imgPath = imgPath.substring(1);
+        else imgPath = path.join(file.dirname!!, imgPath);
+        imgPath = path.normalize(imgPath);
+        onImageFound(file.path!, imgPath);
       }
     }
   }
-
 }
 
 /**
  * dir: Kasten root dir
  * fileRel: file name relative to root dir with extension
  */
-export async function buildMarkdown(dir: string, fileRel: string, noteList: NoteList, onImageFound: ImageFoundListener, quiet: boolean) {
-  const file = path.join(dir, fileRel)
-  const distFile = util.getDistFile(dir, fileRel)
+export async function buildMarkdown(
+  input: vfile.VFile,
+  wikiLinkResolver: (name: string) => (string | undefined)[],
+  onImageFound: ImageFoundListener
+) {
   const docSettings: document.Options = {
     css: [
-      "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css",
+      'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css',
       // "https://unpkg.com/mvp.css"
-      "https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"
+      'https://cdn.jsdelivr.net/npm/water.css@2/out/water.css',
     ],
     style: `
     .katex-display > .katex {
@@ -72,33 +77,23 @@ export async function buildMarkdown(dir: string, fileRel: string, noteList: Note
     .katex .base {
       margin-top: 2px;
       margin-bottom: 2px;
-    }`
-  }
+    }`,
+  };
   const processor = unified()
     .use(markdown)
     .use(gfm)
     .use(math)
     .use(wikiLinkPlugin, {
       hrefTemplate: (permalink: string) => permalink,
-      pageResolver: (name: string) => [
-        noteList.getById(name)?.urlPath
-      ]
+      pageResolver: wikiLinkResolver,
     })
     .use(analyzerAttacker, docSettings, onImageFound)
     .use(remark2rehype, { allowDangerousHtml: true })
     .use(raw)
     .use(katex)
     .use(document, docSettings)
-    .use(html)
+    .use(html);
 
-  const resultFile = await processor.process(
-    await toVfile.read({ path: fileRel, cwd: dir })
-  )
-  const report  = reporter(resultFile, { quiet })
-  if (report.length > 0)
-    console.log(report)
-  resultFile.path = distFile
-
-  await fs.ensureFile(resultFile.path)
-  await toVfile.write(resultFile)
+  const resultFile = await processor.process(input)
+  return resultFile
 }
